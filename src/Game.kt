55f78d924264f45fcb18f1e93ce186293ad6ec05
kos_ykt <kosvasl09@gamil.com>
2021-07-27 22:06:18 +0900

    Task

--- src/Game.kt
+++ src/Game.kt
@@ -1,16 +1,29 @@
 fun main() {
     val name = "Player"
-    var healthPoints = 10
+    var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    if (auraVisible) {
+        println("GREEN")
+    } else {
+        println("NONE")
+    }
 
     if (healthPoints == 100) {
-        println(name + " is in excellent condition!")
+        println("$name is in excellent condition!")
     } else if (healthPoints >= 90) {
-        println(name + " has a few scratches.")
+        println("$name has a few scratches.")
     } else if (healthPoints >= 75) {
-        println(name + " has some minor wounds.")
+        if (isBlessed) {
+            println("$name has some minor wounds but is healing quite quickly!")
+        } else {
+            println("$name has some minor wounds.")
+        }
     } else if (healthPoints >= 15) {
-        println(name + " looks pretty hurt.")
+        println("$name looks pretty hurt.")
     } else {
-        println(name + " is in awful condition!")
+        println("$name is in awful condition!")
     }
 }
