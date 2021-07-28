b701eb352436488daf160007738f7890787d188d
kos_ykt <kosvasl09@gamil.com>
2021-07-28 22:18:26 +0900

    Task

--- src/Game.kt
+++ src/Game.kt
@@ -4,11 +4,20 @@ fun main() {
     val isBlessed = true
     val isImmortal = false
 
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return if (auraVisible) "GREEN" else "NONE"
+}
 
-
-    val healthStatus = when (healthPoints) {
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
+    return when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) {
@@ -19,7 +28,4 @@ fun main() {
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
-    // Состояние игрока
-    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
 }
