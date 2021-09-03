d22ba9bfcfe97d17bdd28c2c86acac3a4c55e942
kos_ykt <kosvasl09@gamil.com>
2021-09-03 20:52:30 +0900

    Task

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,21 +1,32 @@
+const val TAVERN_NAME = "Taernyl's Folly"
+
 fun main() {
-    var swordsJuggling: Int? = null
-    val isJugglingProficient = (1..3).shuffled().last() == 3
-    if (isJugglingProficient) {
-        swordsJuggling = 2
-    }
-    try {
-        proficiencyCheck(swordsJuggling)
-        swordsJuggling = swordsJuggling!!.plus(1)
-    } catch (e: Exception) {
-        println(e)
-    }
-    println("You juggle $swordsJuggling swords!")
+    placeOrder("shandy,Dragon's Breath,5.91")
 }
 
-fun proficiencyCheck(swordsJuggling: Int?) {
-    checkNotNull(swordsJuggling) { "Player cannot juggle swords" }
+fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
 
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
+
+    val phrase = "Ah, delicious $name!"
+    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
 }
-class UnskilledSwordJugglerException() :
-    IllegalStateException("Player cannot juggle swords")
+
+private fun toDragonSpeak(phrase: String){
+    phrase.replace(Regex("[aeiou]")){
+        when(it.value){
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
+}
+
