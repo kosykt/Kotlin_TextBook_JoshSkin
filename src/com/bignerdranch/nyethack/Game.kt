b2e14d6d56864be3fa7fc2385637a680d9628c32
konstantin <kosval09@gmail.com>
2021-09-10 16:13:19 +0900

    Task

--- src/com/bignerdranch/nyethack/Game.kt
+++ src/com/bignerdranch/nyethack/Game.kt
@@ -3,29 +3,54 @@ package com.bignerdranch.nyethack
 import java.io.File
 
 fun main() {
-
-    val player = Player(_name = "Madrigal",
-        healthPoints = 100,
-        isBlessed = true,
-        isImmortal = false)
-    player.castFireball(9)
-
-    var currentRoom = TownSquare()
-    println(currentRoom.description())
-    println(currentRoom.load())
-
-    printPlayerStatus(player)
+    Game.play()
 }
 
-private fun printPlayerStatus(player: Player) {
-    println("(Aura: ${player.auraColor()}) " + "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
-    println("${player.name} ${player.formatHealthStatus()}")
+object Game {
+    val player = Player("Madrigal")
+    var currentRoom: Room = TownSquare()
+
+    init {
+        println("Welcome, adventurer.")
+        player.castFireball(9)
+    }
+
+    fun play() {
+        while (true) {
+            println(currentRoom.description())
+            println(currentRoom.load())
+
+            printPlayerStatus(player)
+
+            print("> Enter your command: ")
+            println(GameInput(readLine()).processCommand())
+        }
+    }
+
+    private fun printPlayerStatus(player: Player) {
+        println("(Aura: ${player.auraColor()}) " + "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+        println("${player.name} ${player.formatHealthStatus()}")
+    }
+
+    private class GameInput(arg: String?) {
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1, { "" })
+
+        fun processCommand() = when (command.lowercase()) {
+            else -> commandNotFound()
+        }
+
+        private fun commandNotFound() = "I'm not quite sure what you're trying to do!"
+    }
 }
 
-class Player (_name: String,
-              var healthPoints: Int = 100,
-              val isBlessed: Boolean,
-              private val isImmortal: Boolean){
+class Player(
+    _name: String,
+    var healthPoints: Int = 100,
+    val isBlessed: Boolean,
+    private val isImmortal: Boolean
+) {
 
     var name = _name
         get() = "${field.lowercase()} of $hometown"
@@ -33,17 +58,21 @@ class Player (_name: String,
             field = value.trim()
         }
 
-    val hometown by lazy {selectHometown()}
+    val hometown by lazy { selectHometown() }
+    var currentPosition = Coordinate(0, 0)
 
     init {
         require(healthPoints > 0) { "healthPoints must be greater than zero." }
         require(name.isNotBlank()) { "Player must have a name." }
     }
 
-    constructor(name: String) : this(name,
+    constructor(name: String) : this(
+        name,
         isBlessed = true,
-        isImmortal = false) {
-        if (name.lowercase() == "kar") healthPoints = 40}
+        isImmortal = false
+    ) {
+        if (name.lowercase() == "kar") healthPoints = 40
+    }
 
     fun auraColor(): String {
         val auraVisible = isBlessed && healthPoints > 50 || isImmortal
@@ -51,16 +80,16 @@ class Player (_name: String,
     }
 
     fun formatHealthStatus() = when (healthPoints) {
-            100 -> "is in excellent condition!"
-            in 90..99 -> "has a few scratches."
-            in 75..89 -> if (isBlessed) {
-                "has some minor wounds, but is healing quite quickly!"
-            } else {
-                "has some minor wounds."
-            }
-            in 15..74 -> "looks pretty hurt."
-            else -> "is in awful condition!"
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds, but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
         }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
 
     fun castFireball(numFireballs: Int = 2) =
         println("A glass of Fireball springs into existence. (x$numFireballs)")
--- src/com/bignerdranch/nyethack/Room.kt
+++ src/com/bignerdranch/nyethack/Room.kt
@@ -13,6 +13,6 @@ class TownSquare: Room("Town Square"){
 
     override val dangerLevel = super.dangerLevel - 3
     private var bellSound = "GWONG"
-    override fun load() = "The villagers rally and cheer as you enter!\n${ringBell()}"
+    final override fun load() = "The villagers rally and cheer as you enter!\n${ringBell()}"
     private fun ringBell() = "The bell tower announces your arrival. $bellSound"
 }
